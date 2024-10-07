104d70baaedaa916e2622f75f4b7c27aedb076f2
roushanku <roushanku@iitbhilai.ac.in>
2024-10-07 17:56:28 +0530

    FEAT: added backend to cross origin

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ const MAX_QUERIES = 3;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://travel-sathi-booking-app-ietk.vercel.app/", credentials: true }));
 
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
